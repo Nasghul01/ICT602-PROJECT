8f130306dfc9d503722af3ade74661bb08bbc977
Nasghul01 <nasrul01arsyad@gmail.com>
2023-01-01 13:34:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ICT602PROJECT
+ A mobile application that implements concept of crowdsourcing information. Application will display news, display map of known hazards on map. The map markers will display type of hazard, time and date the hazarded reported and who reported the hazards.
---
+++ app/src/main/java/com/example/ict602_project/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.example.ict602_project;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    TextView linkTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_about);
+    }
+}
